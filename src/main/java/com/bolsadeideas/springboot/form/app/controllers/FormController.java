67ba09a8a0bc863ba2fc84170e969ca6ac3b025a
JuanEs <12g4ru12@gmail.com>
2020-09-26 15:24:39 -0500

    Cvideo50 se crea el package models.domain, la clase Usuario, el objeto de la clase y se pasan los datos del formulario a la vista resultado.tml

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,10 +34,16 @@ public class FormController {
                                         @RequestParam String password, //  @RequestParam para extraer el parámetro de consulta password.
                                         @RequestParam String email){  //  @RequestParam para extraer el parámetro de consulta email estos tres parametros provienen del formulario.
 
+        Usuario usuario = new Usuario(); // Se Crea una instancia de la clase Usuario
+
+        // Se establecen los parametros de los metodos setter de la clase usuario
+        usuario.setUsername(username);
+        usuario.setPassword(password);
+        usuario.setEmail(email);
+
+        // Se pasa el objeto de la clase usuario a la vista resultado.html
         model.addAttribute("titulo", "Resultado del formulario"); // con el objeto model utilizando clave valor pasamos datos a la vista resultado.html
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+        model.addAttribute("usuario", usuario);
 
         return "resultado";
     }
