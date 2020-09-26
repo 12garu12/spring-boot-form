99efebc20c4bd5f08077058201abe2b93684bb6a
JuanEs <12g4ru12@gmail.com>
2020-09-26 15:42:22 -0500

    Cvideo51 Mapeando los campos del formulario al objeto model

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -24,22 +23,11 @@ public class FormController {
     /**
      * Metodo para la respuesta que procesa los datos que llegan del formulario el formulario
      * @param model objeto de la interfaz model para pasa datos a la vista.
-     * @param username variable que llega del formulario con el nombre del usuario.
-     * @param password variable que llega del formulario con el password del usuario.
-     * @param email variable que llega del formulario con el email del usuario.
      * @return retorna una vista llamada resultado.html con el resultado del procesamiento de los datos.
      */
     @PostMapping("/form")// Anotacion @PostMapping se usa para manejar el tipo de método de solicitud POST.
-    public String procesar(Model model, @RequestParam (name = "username") String username, //  @RequestParam para extraer el parámetro de consulta username.
-                                        @RequestParam String password, //  @RequestParam para extraer el parámetro de consulta password.
-                                        @RequestParam String email){  //  @RequestParam para extraer el parámetro de consulta email estos tres parametros provienen del formulario.
-
-        Usuario usuario = new Usuario(); // Se Crea una instancia de la clase Usuario
-
-        // Se establecen los parametros de los metodos setter de la clase usuario
-        usuario.setUsername(username);
-        usuario.setPassword(password);
-        usuario.setEmail(email);
+    public String procesar(Usuario usuario, Model model){  // para automatizar mas se puede pasar el objeto usuario como
+                                                           // parametro y este a su vez mapea y llena  los atributos de la clase usuario automaticamente.
 
         // Se pasa el objeto de la clase usuario a la vista resultado.html
         model.addAttribute("titulo", "Resultado del formulario"); // con el objeto model utilizando clave valor pasamos datos a la vista resultado.html
--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -6,6 +6,7 @@ public class Usuario {
     private String password;
     private String email;
 
+/*  METODOS SETTER AND GETTER **************************************************************************************/
 
     public String getUsername() {
         return username;
