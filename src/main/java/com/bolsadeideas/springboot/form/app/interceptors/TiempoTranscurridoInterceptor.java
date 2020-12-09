f5bf791ae92095ca0cc67e58b6f9d3883d445b2e
JuanEs <12g4ru12@gmail.com>
2020-12-08 21:14:34 -0500

    CVideo91 Implementando intercertor Tiempo Transcurrido

---
+++ src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -0,0 +1,63 @@
+package com.bolsadeideas.springboot.form.app.interceptors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+
+
+@Component
+public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class); // el logger sirve para poder registrar algun evento en el log o en la traza de la aplicación
+
+    /**
+     * Metodo interceptor para ejecutar una tarea antes de la petición HTTP.
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        LOGGER.info("TiempoTranscurridoInterceptor: preHandle() entrando ... ");
+        long tiempoInicio = System.currentTimeMillis(); // tiempp del sistema actual.
+        request.setAttribute("tiempoInicio", tiempoInicio); // El metodo setAttribute trabaja como el ModelAttribute clave valor, recibe como argumento un String y guarda un tipo Object.
+
+        Random random = new Random(); // Objeto de la clase random para llamar los metodos de la clase random
+        Integer demora = random.nextInt(500); // para emular el tiempo de demora, el argumento crea un numero aleatorio entre 0 y 500;
+        Thread.sleep(demora); //Hace que el subproceso que se está ejecutando se suspenda (cese temporalmente la ejecución) durante el número especificado de milisegundos
+
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        long TiempoFin = System.currentTimeMillis(); // tiempo del sistema actual.
+        long tiempoInicio = (Long) request.getAttribute("tiempoInicio"); // Obtiene el valor del objeto guardado y como es de tipo Object se le hace un cast.
+        long tiempotranscurrido = TiempoFin - tiempoInicio; // Para obtener el tiempo transcurrido restamos el tiempo final menos el inicial.
+
+        if (modelAndView != null){
+            modelAndView.addObject("tiempoTranscurrido", tiempotranscurrido);
+            // modelAndView sirve para pasar datos a la vista con clave valor. Agrega un atributo al modelo.
+            //Parámetros:
+            //attributeName: nombre del objeto que se agregará al modelo (nunca nulo)
+            //atributoValue: objeto para agregar al modelo (puede ser nulo)
+            //Ver también:
+            //ModelMap.addAttribute (Cadena, Objeto), getModelMap ().
+        }
+
+        LOGGER.info("Tiempo transcurrido: ", tiempotranscurrido + "milisegundos");
+        LOGGER.info("TiempoTranscurridoInterceptor postHandle() saliendo ... ");
+
+    }
+}
