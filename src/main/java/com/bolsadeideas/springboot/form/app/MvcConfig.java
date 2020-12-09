2f14b87bf5b8f971fe6083d353f4d248fc7da991
JuanEs <12g4ru12@gmail.com>
2020-12-08 22:42:46 -0500

    Cvideo93 Registrando el interceptor en al configuracion de spring

---
+++ src/main/java/com/bolsadeideas/springboot/form/app/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.bolsadeideas.springboot.form.app;
+
+import com.bolsadeideas.springboot.form.app.interceptors.TiempoTranscurridoInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    @Qualifier("tiempoTranscurridoInterceptor")// Con el nombre del interceptor por si existe mas de uno
+    private HandlerInterceptor tiempoTranscurridoInterceptor; // inyeccion de dependencia de la clase TiempoTranscurridoInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tiempoTranscurridoInterceptor); //se agrega el interceptor y las rutas del controlador para el interceptor.
+    }
+}
--- src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.form.app.interceptors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Random;
 
 
-@Component
+@Component("tiempoTranscurridoInterceptor") // Con el nombre para identificar este interceptor
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class); // el logger sirve para poder registrar algun evento en el log o en la traza de la aplicación
@@ -27,7 +28,13 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        if(handler instanceof HandlerMethod){ // con la sentencia if se le pregunta si es una instancia de alguna clase
+            HandlerMethod method = (HandlerMethod) handler; // se le hace un cast por que el objeto handler es de tipo Object.
+            LOGGER.info("Es un metodo del controlador: " + method.getMethod().getName()); // para saber el nombre del metodo del controlador
+        }
+
         LOGGER.info("TiempoTranscurridoInterceptor: preHandle() entrando ... ");
+        LOGGER.info("Interceptando: " + handler); // para ver alguna informacion en la consola
         long tiempoInicio = System.currentTimeMillis(); // tiempp del sistema actual.
         request.setAttribute("tiempoInicio", tiempoInicio); // El metodo setAttribute trabaja como el ModelAttribute clave valor, recibe como argumento un String y guarda un tipo Object.
 
@@ -46,7 +53,7 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
         long tiempoInicio = (Long) request.getAttribute("tiempoInicio"); // Obtiene el valor del objeto guardado y como es de tipo Object se le hace un cast.
         long tiempotranscurrido = TiempoFin - tiempoInicio; // Para obtener el tiempo transcurrido restamos el tiempo final menos el inicial.
 
-        if (modelAndView != null){
+        if (handler instanceof HandlerMethod && modelAndView != null){
             modelAndView.addObject("tiempoTranscurrido", tiempotranscurrido);
             // modelAndView sirve para pasar datos a la vista con clave valor. Agrega un atributo al modelo.
             //Parámetros:
@@ -56,7 +63,7 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
             //ModelMap.addAttribute (Cadena, Objeto), getModelMap ().
         }
 
-        LOGGER.info("Tiempo transcurrido: ", tiempotranscurrido + "milisegundos");
+        LOGGER.info("Tiempo transcurrido: " + tiempotranscurrido + " milisegundos");
         LOGGER.info("TiempoTranscurridoInterceptor postHandle() saliendo ... ");
 
     }
